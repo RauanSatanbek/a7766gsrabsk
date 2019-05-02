8df5cee73560bab168070172f7a9b4bfb143d943
Rauan <rauan.satanbek@gmail.com>
2019-05-02 18:32:23 +0600

    Project: Online/Offline Music Player of Singer

---
+++ app/src/main/java/i/am/rauan/satanbek/a7766gsrabsk/MainActivity.kt
@@ -0,0 +1,12 @@
+package i.am.rauan.satanbek.a7766gsrabsk
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
